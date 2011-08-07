2aaa567b54187410472f1d06923b0d95c5d41b59
Brett Profitt <brett.profitt@gmail.com>
2011-08-07 12:16:00 -0700

    Added objects, metadata, and annotation counts.

--- views/default/bulk_user_admin/user.php
+++ views/default/bulk_user_admin/user.php
@@ -17,12 +17,27 @@ $checkbox = "<input type=\"checkbox\" name=\"bulk_user_admin_guids[]\" value=\"$
 $first_login = elgg_view_friendly_time($user->time_created);
 $last_login = elgg_view_friendly_time($user->last_login);
 $last_action = elgg_view_friendly_time($user->last_action);
+$objects = elgg_get_entities(array(
+	'owner_guid' => $user->guid,
+	'count' => true
+));
+
+$db_prefix = get_config('dbprefix');
+
+$q = "SELECT COUNT(id) as count FROM {$db_prefix}annotations WHERE owner_guid = $user->guid";
+$data = get_data($q);
+$annotations = (int) $data[0]->count;
+
+$q = "SELECT COUNT(id) as count FROM {$db_prefix}metadata WHERE owner_guid = $user->guid";
+$data = get_data($q);
+$metadata = (int) $data[0]->count;
 
 // the CSS for classless <label> is really, really annoying.
 $info = <<<___HTML
 <label style="font-size: inherit; font-weight: inherit; color: inherit;">
-<p>$checkbox $user->name | $user->username | $user->email</p>
+<p>$checkbox $user->name | $user->guid | $user->username | $user->email</p>
 <p>Last login: $last_login | First login: $first_login | Last action: $last_action</p>
+<p>Objects: $objects | Annotations: $annotations | Metadata: $metadata</p>
 ___HTML;
 
 if ($banned) {
